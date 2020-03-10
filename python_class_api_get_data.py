ca6cdaf45557aac0a04d869d9c4bd1dd827508af
viache19 <53226747+viache19@users.noreply.github.com>
2020-03-10 13:26:00 +0100

    Add files via upload

---
+++ python_class_api_get_data.py
@@ -0,0 +1,88 @@
+import requests
+import json
+from python_variable import devolver_variables_globales
+import numpy
+    #para saber el tipo
+    #print(type(jsontoken_descodificar_a_string))
+
+    #Hacer la llamada a la <class dict>
+    #print(jsontoken_descodificar_a_string["access_token"])
+
+#dev = devolver_variables_globales()
+#print(dev.url_vg)
+
+
+
+class authenticate:
+    def __callauth__(self):
+        dev = devolver_variables_globales()
+        self.url = dev.url_vg
+        self.username = dev.username_vg
+        self.password = dev.password_vg
+
+        d = {'user' : self.username, 'password' : self.password}
+
+        response = requests.post("http://" + self.url + "/pbxapi/authenticate", data=d)
+        if response.status_code == 200:
+
+            datos_json = json.dumps(response.json())
+            jsontoken_descodificar_a_string = json.loads(datos_json)
+            numpy.save("temptoken.npy", jsontoken_descodificar_a_string)
+
+        else:
+            print("We had a problem with connection to the Issabel PBXAPI")
+
+    def __call__(self):
+        try:
+            dev = devolver_variables_globales()
+            self.url = dev.url_vg
+            self.username = dev.username_vg
+            self.password = dev.password_vg
+
+            token_data_load = numpy.load("temptoken.npy", allow_pickle=True)
+
+            hed = {'Authorization': 'Bearer ' + token_data_load.item().get("access_token")}
+
+            response = requests.get("http://"+ dev.url_vg +"/pbxapi/extensions", headers=hed)
+
+            if response.status_code == 200:
+
+                datos_json = json.dumps(response.json())
+                extensions = json.loads(datos_json)
+
+                if extensions["status"] == "success":
+
+                    def jprint(parsed):
+                        text = json.dumps(parsed, indent=4)
+                        print(text)
+
+                    jprint(response.json())
+
+                elif extensions["status"] == "expired":
+                    bearertokencall(autentificacion())
+
+                else:
+                    print("We had a problem with the access to Issabel PBXAPI")
+
+            else:
+
+                print("We had a problem with connection to the Issabel PBXAPI")
+        except requests.exceptions.ConnectionError:
+            print("Sorry, we have a connection error")
+
+
+#p1 = authenticate()
+#p1.__call__()
+#authenticate().__call__(authenticate().__callauth__())
+
+#SE SUPONE QUE ESTA FUNCION SE LLAMA LA PRIMERA
+#authenticate().__callauth__()
+
+authenticate().__call__()
+
+#class BearerAuth(requests.auth.AuthBase):
+#    def __init__(self, token):
+#        self.token = token
+#    def __call__(self, r):
+#        r.headers["authorization"] = "Bearer " + self.token
+#        return r
---
+++ python_variable.py
@@ -0,0 +1,8 @@
+class variables_globales:
+    def __init__(self):
+        self.url_vg="192.168.6.50"
+        self.username_vg="admin"
+        self.password_vg="Av_Y7wov0"
+
+def devolver_variables_globales():
+    return variables_globales()
